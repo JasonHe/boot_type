044ef9a6d91d3b70eda8f9e170a37f51acd8f9d1
Jason He <59273498+JasonHe@users.noreply.github.com>
2022-05-15 16:14:07 +0800

    Update boot_type.sh

--- boot_type.sh
+++ boot_type.sh
@@ -1,8 +1,2 @@
 #! bin/bash
-dmesg | grep -q "EFI v"    # -q tell grep to output nothing
-if [ $? -eq 0 ]      # check exit code; if 0 EFI, else BIOS
-then
-    echo "You are using EFI boot."
-  else
-    echo "You are using BIOS boot"
-fi
+[ -d /sys/firmware/efi ] && echo "启动类型为UEFI" || "启动类型为BIOS"
